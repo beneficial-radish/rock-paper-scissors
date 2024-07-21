b965f77f3422dcb417c67c8b05b1762864d9f34c
beneficial-radish <144536648+beneficial-radish@users.noreply.github.com>
2024-07-20 21:25:13 -0500

    Write pseudocode for program and instructions on webpage

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("Hello World!")
+
+// Rock Paper Scissors
+//Prompt user to choose between rock, paper, and scissors
+//Randomly choose between the integers 1-3 
+//If 1, computer chooses rock. If 2, computer chooses paper. If 3, computer chooses scissors
+//plot on paper which scenarios result in computer winning and which scenarios result in user winning
+//subtract values of computer from user and determine if there's a way to plot each difference to one outcome
+//if possible, set the if statement such that values of difference are grouped into "win" and "lose" conditions
+//if win values, say, "you win! again?" and add 1 to wins if loss values say, "you lose! again?" and add one to losses
+//call function score with command "score" 
+//Show wins and losses as "wins: num losses: num"
+//
+//
+//
+//
+//
