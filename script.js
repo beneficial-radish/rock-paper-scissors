194c0778fa3d3b23266fc867578749942dd67357
beneficial-radish <144536648+beneficial-radish@users.noreply.github.com>
2024-07-22 00:35:05 -0500

    Code is functional but loop still not debugged

--- script.js
+++ script.js
@@ -2,62 +2,86 @@ console.log("Hello World!")
 
 // Rock Paper Scissors
 let computerChoice;
+let userChoiceString;
 let userChoice;
 let result;
 let computerChoiceString;
-let wins;
-let losses;
+let wins = 0;
+let losses = 0;
 
 //Prompt user to choose between rock, paper, and scissors
 //Assign value to user's choice. Rock is 1, paper is 2, scissors is 3
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
 
 
   //gets user's choice and subtracts random computer choice value between 1-3
-  readline.question('Choose rock, paper, or scissors.', name => {
-    name = rock ? userChoice = 1 : name = paper? userChoice = 2 : name = scissors ? userChoice = 3 : console.log("please try again. Type rock, paper, or scissors")
-    readline.close();
-    computerChoice = random1to3()
-    result = userChoice - computerChoice;
-    return result;
-  });
+  function start() {
+  userChoiceString = prompt("Choose rock, paper, or scissors!") 
+  }
+  function takeInput() {
+    if(userChoiceString == "rock" || userChoiceString == "paper" || userChoiceString == "scissors") {
+        userChoiceString == "rock" ? userChoice = 1 : userChoiceString == "paper"? userChoice = 2 : userChoiceString == "scissors" ? userChoice = 3 : userChoice = 0
+    } else if (userChoiceString == "wins") {
+        console.log(wins) 
+    } else if (userChoiceString == "losses") {
+     console.log(losses) 
+    } else {
+    console.log("Please try again. Type rock, paper, or scissors.")
+    }
+    return userChoice
+  }
 
+  
 //If 1, computer chooses rock. If 2, computer chooses paper. If 3, computer chooses scissors
 //randomly choose between integers 1-3. 
 function random1to3() {
     computerChoice = Math.floor(Math.random() * 3 + 1)
-    stringify()
+   return computerChoice
 }
-    return computerChoice
 
 function stringify() {
-    computerChoice = 1 ? computerChoiceString = "rock" : computerChoice = 2 ? computerChoiceString = "paper" : computerChoice = 3 ? computerChoiceString = "scissors" : computerChoiceString = shoes;
+    computerChoice == 1 ? computerChoiceString = "rock" : computerChoice == 2 ? computerChoiceString = "paper" : computerChoice == 3 ? computerChoiceString = "scissors" : computerChoiceString = shoes;
 
     return computerChoiceString
 }
 
+function calculate() {
+    if (userchoice > 0) {
+        result = userChoice - computerChoice
+    }
+    else {
+    start ()
+    }
+    return result
+}
 //value 0 means tie, values 1 and -2 mean win, and values -1 and 2 mean loss
-
+//add up losses and wins as they happen
 
 function winLose() {
     if (result === 0) {
         console.log(`It's a tie! we both chose ${computerChoiceString}`)
     } else if (result === 1 || result === -2) {
-        console.log(`You win! I chose $(computerChoiceString)`)
+        console.log(`You win! I chose ${computerChoiceString}`)
+        wins ++
     } else if (result === -1 || result === 2) {
-        console.log(`You lose! I chose $(computerChoiceString)`)
+        console.log(`You lose! I chose ${computerChoiceString}`)
+        losses ++
     } else {
         console.log(`idk what you did, but you screwed up.`)
     }
-
 }
 
+//now loop it
 
+for (let i = 0; i < 100; i++) {
+    start();
+takeInput();
+random1to3();
+stringify();
+calculate();
+winLose();
 
-//if possible, set the if statement such that values of difference are grouped into "win" and "lose" conditions
+  }
+
 //if win values, say, "you win! again?" and add 1 to wins if loss values say, "you lose! again?" and add one to losses
 //call function score with command "score" 
 //Show wins and losses as "wins: num losses: num"
