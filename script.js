792376a49ced77958890ce4df9fc110e94b46322
beneficial-radish <144536648+beneficial-radish@users.noreply.github.com>
2024-07-21 23:47:44 -0500

    create a bunch of functions

--- script.js
+++ script.js
@@ -1,39 +1,62 @@
 console.log("Hello World!")
 
 // Rock Paper Scissors
+let computerChoice;
+let userChoice;
+let result;
+let computerChoiceString;
+let wins;
+let losses;
 
 //Prompt user to choose between rock, paper, and scissors
-console.log("Choose rock(), paper(), or scissors()")
+//Assign value to user's choice. Rock is 1, paper is 2, scissors is 3
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
 
-//new step: if user inputs "rock", "paper", or "scissors"...
-//actually, should these be three separate functions? Since it's being played in the console instead of with a selector.
-//separate function way
+
+  //gets user's choice and subtracts random computer choice value between 1-3
+  readline.question('Choose rock, paper, or scissors.', name => {
+    name = rock ? userChoice = 1 : name = paper? userChoice = 2 : name = scissors ? userChoice = 3 : console.log("please try again. Type rock, paper, or scissors")
+    readline.close();
+    computerChoice = random1to3()
+    result = userChoice - computerChoice;
+    return result;
+  });
 
 //If 1, computer chooses rock. If 2, computer chooses paper. If 3, computer chooses scissors
-//randomly choose between integers 1-3. But it favors 1 and 3 over 2, it seems
+//randomly choose between integers 1-3. 
 function random1to3() {
-    let computerChoice = Math.floor(Math.random() * 3 + 1)
+    computerChoice = Math.floor(Math.random() * 3 + 1)
+    stringify()
+}
     return computerChoice
-}
-//but now I have a new issue. It's not super sleek to call functions with empty parentheses.
-//it would look cuter if it was play(rock) or play(paper) or play(scissors)
-//in terms of a game. Or else I should just make a selector with buttons. 
 
-//generates a score to determine winning or losing. 
-function rock() {
-    const userChoiceRock = 1
-    let computerChoiceRock = random1to3()
-    let decider = computerChoiceRock - userChoiceRock
+function stringify() {
+    computerChoice = 1 ? computerChoiceString = "rock" : computerChoice = 2 ? computerChoiceString = "paper" : computerChoice = 3 ? computerChoiceString = "scissors" : computerChoiceString = shoes;
+
+    return computerChoiceString
 }
 
-//I don't like this as separate functions. 
+//value 0 means tie, values 1 and -2 mean win, and values -1 and 2 mean loss
+
+
+function winLose() {
+    if (result === 0) {
+        console.log(`It's a tie! we both chose ${computerChoiceString}`)
+    } else if (result === 1 || result === -2) {
+        console.log(`You win! I chose $(computerChoiceString)`)
+    } else if (result === -1 || result === 2) {
+        console.log(`You lose! I chose $(computerChoiceString)`)
+    } else {
+        console.log(`idk what you did, but you screwed up.`)
+    }
+
+}
 
 
 
-
-
-//plot on paper which scenarios result in computer winning and which scenarios result in user winning
-//subtract values of computer from user and determine if there's a way to plot each difference to one outcome
 //if possible, set the if statement such that values of difference are grouped into "win" and "lose" conditions
 //if win values, say, "you win! again?" and add 1 to wins if loss values say, "you lose! again?" and add one to losses
 //call function score with command "score" 
